039ecc1f68da8ab1a9694d40c5f7c6ceacc94b62
Eliezer Pearl <eliezer.pearl@gmail.com>
2016-09-15 17:31:03 -0400

    added a tank drive thaat

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeRegister.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeRegister.java
@@ -81,6 +81,7 @@ public class FtcOpModeRegister implements OpModeRegister {
         /**
          * Any manual OpMode class registrations should go here.
          */
-        manager.register("TestTable", OutreachTankDrive.class);
+        manager.register("TestTable", DumbTankDrive.class);
+
     }
 }
