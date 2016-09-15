d44284c75eb6ce9fefb2bddf87cebc5e46522567
Eliezer Pearl <eliezer.pearl@gmail.com>
2016-09-15 16:46:47 -0400

    registered 4realz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeRegister.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeRegister.java
@@ -81,6 +81,6 @@ public class FtcOpModeRegister implements OpModeRegister {
         /**
          * Any manual OpMode class registrations should go here.
          */
-
+        manager.register("TestTable", OutreachTankDrive.class);
     }
 }
