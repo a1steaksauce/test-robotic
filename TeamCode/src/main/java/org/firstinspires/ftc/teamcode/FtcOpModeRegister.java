b38f55cc9ed40c257c5a31b14af17818e37712ff
Eliezer Pearl <eliezer.pearl@gmail.com>
2016-09-25 14:54:24 -0400

    smok wed erydae

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeRegister.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeRegister.java
@@ -82,6 +82,7 @@ public class FtcOpModeRegister implements OpModeRegister {
          * Any manual OpMode class registrations should go here.
          */
         manager.register("TestTable", DumbTankDrive.class);
+        manager.register("SICKASSMEMER", Sensors.class);
 
     }
 }
