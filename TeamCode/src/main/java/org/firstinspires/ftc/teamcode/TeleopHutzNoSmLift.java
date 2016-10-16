982ac041e86d2f7e7faa7a02dcc09ab4ee5398bc
Eliezer Pearl <eliezer.pearl@gmail.com>
2016-10-16 10:30:31 -0400

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopHutzNoSmLift.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Eliezer Pearl on 10/16/2016.
+ */
+
+public class TeleopHutzNoSmLift extends LinearOpMode {
+    public void runOpMode(){
+        //TODO: Code
+    }
+}
