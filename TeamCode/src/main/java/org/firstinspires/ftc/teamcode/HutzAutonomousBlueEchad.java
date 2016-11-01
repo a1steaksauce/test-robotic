6cbac16793cfd949a52488554da185fd3a859d52
Ben <bdkotton@gmail.com>
2016-11-01 17:06:01 -0400

    Oops- one last edit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HutzAutonomousBlueEchad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HutzAutonomousBlueEchad.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.En
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
