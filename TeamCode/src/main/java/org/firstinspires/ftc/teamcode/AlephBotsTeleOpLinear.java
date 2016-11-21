721afe0b9ddda35d1a52145af067d10804274080
Aaron <aaronkbutler@icloud.com>
2016-11-21 10:23:46 -0500

    Edited Aleph Bots code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlephBotsTeleOpLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlephBotsTeleOpLinear.java
@@ -100,6 +100,7 @@ public class AlephBotsTeleOpLinear extends LinearOpMode{
             if (gamepad1.y){
                 RTouchServo.setPosition(0.0);
                 LTouchServo.setPosition(1.0);
+                ButtonPresser.setPosition(0.35);
             }
             //TELEMETRY
             telemetry.addData("Aleph Bots Robot:", "Running!");
