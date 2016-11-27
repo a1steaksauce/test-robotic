3d6ed9e9836fa8e18b560d5d5c6845d47f0ae3bf
Eliezer Pearl <eliezer.pearl@gmail.com>
2016-11-27 14:00:03 -0500

    ben forgot to sync his autonomous so I made my own

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HutzAll.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by poop on 11/27/2016.
+ */
+
+public class HutzAll extends LinearOpMode{
+    DcMotor
+    public void runOpMode(){
+
+    }
+}
